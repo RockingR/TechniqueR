dbcf2e2d553c08044d9bc892310560ebb2ab922c
RockingR <kennyliaocan@gmail>
2015-01-22 00:13:06 +0800

    finished Rdebug

---
+++ R dummy/pastePercent.R
@@ -0,0 +1,7 @@
+x <- c(0.458, 1.6653, 0.83112) # data created
+
+percent <- round(x * 100, digits = 1) # perform calculation
+
+result <- paste(percent, "%", sep = "") # make appearance for result
+
+print(result) # display the result and then save it as .R file through Rstudio
